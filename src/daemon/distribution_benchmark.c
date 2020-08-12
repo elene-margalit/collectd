5cdb6a5613a9e5f462c1ae6b116cbede856fd564
elene-margalit <elene.margalit@gmail.com>
2020-08-12 08:02:57 +0200

    Added distribution_t with exponential and custom constructors to benchmark.

--- src/daemon/distribution_benchmark.c
+++ src/daemon/distribution_benchmark.c
@@ -3,8 +3,23 @@
 #include <stdio.h>
 #include <time.h>
 
+double calculate_gauge(size_t i, int buckets_number, double buckets_size) {
+    double gauge;
+    if(i%3 == 0) {
+        gauge = rand() % (buckets_number * (int) buckets_size + (int) buckets_size) + 1;
+    }
+    else if(i%3 == 1) {
+        gauge = rand() % (int) (buckets_number * 3 * pow(2, buckets_number)) + 1;
+    }
+    else {
+        gauge = rand() % (buckets_number * 100) + 1;
+    }
+    return gauge;
+}
+
 int main(int argc, char **argv) {
     int iterations = 20000000;
+    double gauge = 0;
     if (argc < 2) {
         printf("No bucket number found.\n");
         exit(EXIT_FAILURE);
@@ -14,11 +29,23 @@ int main(int argc, char **argv) {
     double buckets_size = 25;
     printf("%d, ", buckets_number);
 
-    distribution_t *dist_test = distribution_new_linear(buckets_number, buckets_size);
+    double *custom = malloc(sizeof(double) * buckets_number - 1); 
+    custom[0] = rand() % (100 + 1);
+    for (size_t i = 1; i < buckets_number - 1; i++) {
+        custom[i] = custom[i - 1] + rand() % 100 + 1;
+    }
+
+    const int dist_number = 3;
+    distribution_t *dists[dist_number];
+    dists[0] = distribution_new_linear(buckets_number, buckets_size);
+    dists[1] = distribution_new_exponential(buckets_number, 3, 2);
+    dists[2] = distribution_new_custom(buckets_number - 1, custom);
+
     struct timespec start_update;
     clock_gettime(CLOCK_MONOTONIC, &start_update);
     for(size_t i = 0; i < iterations; i++) {
-        distribution_update(dist_test, rand() % (buckets_number * (int) buckets_size + (int) buckets_size + 1));
+        gauge = calculate_gauge(i, buckets_number, buckets_size);
+        distribution_update(dists[i%dist_number], gauge);
     }
     struct timespec end_update;
     clock_gettime(CLOCK_MONOTONIC, &end_update);
@@ -28,7 +55,7 @@ int main(int argc, char **argv) {
     struct timespec start_percentile;
     clock_gettime(CLOCK_MONOTONIC, &start_percentile);
     for(size_t i = 0; i < iterations; i++) {
-        distribution_percentile(dist_test, rand() % (100 + 1));
+        distribution_percentile(dists[i%dist_number], rand() % (100 + 1));
     }
     struct timespec end_percentile;
     clock_gettime(CLOCK_MONOTONIC, &end_percentile);
@@ -38,7 +65,8 @@ int main(int argc, char **argv) {
     struct timespec start_mixed;
     clock_gettime(CLOCK_MONOTONIC, &start_mixed);
     for(size_t i = 0; i < iterations; i++) {
-        i%10 == 0 ? distribution_percentile(dist_test, rand() % (100 + 1)) : distribution_update(dist_test, rand() % (800 + 1));
+        gauge = calculate_gauge(i, buckets_number, buckets_size);
+        i%10 == 0 ? distribution_percentile(dists[i%dist_number], rand() % (100 + 1)) : distribution_update(dists[i%dist_number], gauge);
     }
     struct timespec end_mixed;
     clock_gettime(CLOCK_MONOTONIC, &end_mixed);
